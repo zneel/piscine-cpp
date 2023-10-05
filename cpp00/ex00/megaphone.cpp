38d273fcaeb479b72281d8b8b0c0a24cb5188a3d
Elliot Bouvier <ebouvier@student.42.fr>
2023-10-05 15:32:13 +0200

    better c++

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: ebouvier <ebouvier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 13:52:06 by ebouvier          #+#    #+#             */
-/*   Updated: 2023/09/23 13:52:35 by ebouvier         ###   ########.fr       */
+/*   Updated: 2023/10/05 15:31:55 by ebouvier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,22 @@
 #include <iostream>
 #include <string>
 
-#define DEFAULT "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
+int main(int argc, const char **argv) {
+  const std::string yell = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+  if (argc == 1) {
+    std::cout << yell << std::endl;
+    return (0);
+  }
 
-int	main(int argc, const char **argv)
-{
-	if (argc == 1)
-	{
-		std::cout << DEFAULT << std::endl;
-		return (0);
-	}
-	std::string str;
-	for (int i = 1; i < argc; ++i)
-	{
-		str += argv[i];
-		if (i < argc - 1)
-			str += ' ';
-	}
-	for (std::string::iterator it = str.begin(); it != str.end(); ++it)
-	{
-		*it = std::toupper(*it);
-	}
-	std::cout << str << std::endl;
-	return (0);
+  std::string str;
+  for (int i = 1; i < argc; ++i) {
+    str.append(argv[i]);
+    if (i < argc - 1)
+      str.append(" ");
+  }
+  for (std::string::iterator it = str.begin(); it != str.end(); ++it) {
+    *it = std::toupper(*it);
+  }
+  std::cout << str << std::endl;
+  return (0);
 }
