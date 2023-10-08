ae1eb57b849ee77b65ec6857edec60358f286679
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-10-08 21:53:11 +0200

    harl

---
+++ cpp01/ex05/main.cpp
@@ -0,0 +1,16 @@
+#include "Harl.hpp"
+
+int main() {
+  Harl logger;
+  std::cout << "DEBUG LEVEL:" << std::endl;
+  logger.complain("DEBUG");
+  std::cout << "INFO LEVEL:" << std::endl;
+  logger.complain("INFO");
+  std::cout << "WARNING LEVEL:" << std::endl;
+  logger.complain("WARNING");
+  std::cout << "ERROR LEVEL:" << std::endl;
+  logger.complain("ERROR");
+  std::cout << "UNKNOWN LEVEL:" << std::endl;
+  logger.complain("toto");
+  return 0;
+}
