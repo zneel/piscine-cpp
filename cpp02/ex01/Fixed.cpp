f859bc54fd900af32724cc9ffae2597318fc5225
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-10-11 19:45:56 +0200

    add main test

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -1,31 +1,67 @@
 #include "Fixed.hpp"
 
+/**
+ * @brief Construct a new Fixed::Fixed object
+ *
+ */
 Fixed::Fixed() : fixedPoint_(0 * (1 << this->fractionalBits_)) {
   std::cout << "Default constructor called" << std::endl;
 }
 
+/**
+ * @brief Construct a new Fixed::Fixed from integer
+ *
+ * @param i
+ */
 Fixed::Fixed(int const i) : fixedPoint_(i * (1 << this->fractionalBits_)) {
   std::cout << "Integer constructor called" << std::endl;
 }
 
+/**
+ * @brief Construct a new Fixed::Fixed from floating point
+ *
+ * @param f
+ */
 Fixed::Fixed(float const f)
     : fixedPoint_(roundf(f * (1 << this->fractionalBits_))) {
   std::cout << "Float constructor called" << std::endl;
 }
 
-Fixed::Fixed(Fixed const &other) : fixedPoint_(other.getRawBits()) {
+/**
+ * @brief Copy constructor
+ *
+ * @param other
+ */
+Fixed::Fixed(Fixed const &other) : fixedPoint_(other.fixedPoint_) {
   std::cout << "Copy constructor called" << std::endl;
 }
 
+/**
+ * @brief Overload assignment operator
+ *
+ * @param other
+ * @return Fixed&
+ */
 Fixed &Fixed::operator=(const Fixed &other) {
   if (this == &other) return *this;
   std::cout << "Copy assignment operator called" << std::endl;
-  this->fixedPoint_ = other.getRawBits();
+  this->fixedPoint_ = other.fixedPoint_;
   return *this;
 }
 
+/**
+ * @brief Destructor
+ *
+ */
 Fixed::~Fixed() { std::cout << "Destructor called" << std::endl; }
 
+/**
+ * @brief Overload << operator to print the fixed-point value
+ *
+ * @param os
+ * @param fp
+ * @return std::ostream&
+ */
 std::ostream &operator<<(std::ostream &os, Fixed const &fp) {
   os << fp.toFloat();
   return os;
@@ -38,8 +74,18 @@ int Fixed::getRawBits() const {
 
 void Fixed::setRawBits(int const raw) { this->fixedPoint_ = raw; }
 
+/**
+ * @brief Convert the fixed-point value to floating point value
+ *
+ * @return float
+ */
 float Fixed::toFloat() const {
   return static_cast<float>(this->fixedPoint_) / (1 << this->fractionalBits_);
 }
 
+/**
+ * @brief Convert the fixed-point value to integer value
+ *
+ * @return int
+ */
 int Fixed::toInt() const { return this->fixedPoint_ >> this->fractionalBits_; };
--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -8,13 +8,15 @@ int main(void) {
   Fixed const c(42.42f);
   Fixed const d(b);
   a = Fixed(1234.4321f);
+
+  // Test << operator
   std::cout << "a is " << a << std::endl;
-  std::cout << "b is " << b << std::endl;
-  std::cout << "c is " << c << std::endl;
-  std::cout << "d is " << d << std::endl;
+
+  // Test fixed-point value to integer value
   std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-  std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-  std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-  std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+  // Test fixed-point value to floating point value
+  std::cout << "a is " << a.toFloat() << " as floating point" << std::endl;
+
   return 0;
 }
