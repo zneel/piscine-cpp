0c85de57fb1c91ff695b239b66d6c9c62c6a7075
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-10-18 21:58:27 +0200

    wip

---
+++ cpp06/ex01/main.cpp
@@ -0,0 +1,20 @@
+#include "Serializer.hpp"
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+    {
+        std::cout << "Usage: ./convert [string]" << std::endl;
+        return 1;
+    }
+
+    try
+    {
+        ScalarConverter::convert(av[1]);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return 0;
+}
