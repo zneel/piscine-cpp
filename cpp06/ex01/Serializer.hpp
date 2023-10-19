2eea113ea9a7908de3401f9e283075e3c27cc5b3
Elliot Bouvier <elliotbouvier@protonmail.com>
2023-10-19 18:18:47 +0200

    serializer

--- cpp06/ex01/Serializer.hpp
+++ cpp06/ex01/Serializer.hpp
@@ -1,5 +1,24 @@
 #pragma once
 
+#include <stdint.h>
+#include <string>
+
+struct Data
+{
+    std::string street;
+    std::string name;
+};
+
 class Serializer
 {
+
+  private:
+    Serializer(Serializer const &);
+    Serializer &operator=(Serializer const &);
+    ~Serializer();
+    Serializer();
+
+  public:
+    static uintptr_t serialize(Data *ptr);
+    static Data *deserialize(uintptr_t raw);
 };
--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -1,20 +1,14 @@
 #include "Serializer.hpp"
+#include <iostream>
 
-int main(int ac, char **av)
+int main()
 {
-    if (ac != 2)
-    {
-        std::cout << "Usage: ./convert [string]" << std::endl;
-        return 1;
-    }
-
-    try
-    {
-        ScalarConverter::convert(av[1]);
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    return 0;
+    struct Data data = {"e1", "norminet"};
+    uintptr_t raw = Serializer::serialize(&data);
+    std::cout << "raw: 0x" << std::hex << raw << std::endl;
+    Data *ptr = Serializer::deserialize(raw);
+    std::cout << "ptr: " << ptr << std::endl;
+    std::cout << "ptr->street: " << ptr->street << std::endl;
+    std::cout << "ptr->name: " << ptr->name << std::endl;
+    return (0);
 }
